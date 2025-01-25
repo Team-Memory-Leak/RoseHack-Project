c9d506d1d6ed384b4e7dfe14fee195a03f432225
mallowford <mallowford@gmail.com>
2025-01-25 15:09:09 -0800

    Added fonts Added licenses Fixed main.py

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
+from pygame.locals import *
+import pygame.freetype
 
+# Initialize Pygame
 pygame.init()
-width = 800
-height = 600
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-running = True
+pygame.freetype.init()
 
+pygame.display.set_caption("RoseHack Project")
+
+gameFont = pygame.freetype.Font("fonts/SourGummy.ttf", 24)
+
+# Set up the drawing window
+intiailWidth = 800
+initialHeight = 600
+screen = pygame.display.set_mode((initialHeight, intiailWidth))
+
+# Run until the user asks to quit
+running = True
 while running:
+
+  # Did the user click the window close button?
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-  clock.tick(60)
 
-pygame.quit
+  # Fill the background with white
+  screen.fill((255, 255, 255))
+
+  # Draw a solid blue circle in the center
+  pygame.draw.circle(screen, (0, 0, 255), (intiailWidth//2, initialHeight//2), 75)
+
+  # Flip the display
+  pygame.display.flip()
+
+pygame.freetype.quit()
+pygame.quit()
