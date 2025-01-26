ae6aff19ae5978745a7cd016c97273eadb3ffc62
lbojo006 <luisbojorquezgoal@gmail.com>
2025-01-26 00:44:27 -0800

    Combined Both Code but fusion of implementation is still needed

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import pygame
 import random
 
+# Initialize Pygame
+pygame.init()
+pygame.freetype.init()
+
+pygame.display.set_caption("RoseHack Project")
+
 englishDictionary = open("diction/EnglishDictionary", "r")
 imageHash = open("diction/ImageHash", "r")
 japaneseDictionary = open("diction/JapaneseDictionary", "r")
@@ -11,21 +17,14 @@ japaneseDictionary = japaneseDictionary.read().split("\n")
 
 count = 0
 historyIndex = -1
-
+  
 gameFont = pygame.freetype.Font("fonts/SourGummy.ttf", 24)
 gameFont2 = pygame.freetype.Font("fonts/KiwiMaru.ttf", 40)
 
 
-while count != 2:
-  correlatedIndex = random.randint(0, len(englishDictionary))
-  while correlatedIndex == historyIndex:
-    correlatedIndex = random.randint(0, len(englishDictionary))
-  print(imageHash[correlatedIndex])
-  print(japaneseDictionary[correlatedIndex])
-
-  option1 = random.randint(0, len(englishDictionary) - 1)
-  option2 = random.randint(0, len(englishDictionary) - 1)
-  
+#Inititial window size 
+width = 1600
+height = 1150
 # Create a resizable display
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption("RoseHack Project")
@@ -33,28 +32,11 @@ clock = pygame.time.Clock()
 running = True
 score = 0
 button_visible = True
+background = pygame.image.load("Images/Rosehack Bg Project.png")
 
+background = pygame.transform.scale(background, (width, height))
 
-  while option1 == correlatedIndex or option1 == historyIndex:
-    option1 = random.randint(0, len(englishDictionary) - 1)
-  while option2 == correlatedIndex or option2 == option1 or option2 == historyIndex:
-    option2 = random.randint(0, len(englishDictionary) - 1)
-
-  print("Which of the following is the correct translation of the word above?")
-
-  possibleAnswers = [englishDictionary[correlatedIndex],englishDictionary[option1], englishDictionary[option2]]
-  random.shuffle(possibleAnswers)
-
-  print(possibleAnswers[0])
-  print(possibleAnswers[1])
-  print(possibleAnswers[2])
-
-  count += 1
-  historyIndex = correlatedIndex
-
-
-
-def defaultButton(width, height, x, y, text, color, hover_color, font):
+def defaultButton(width, height, x, y, text, color, hover_color, font, font_size):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     global score, button_visible
@@ -74,7 +56,7 @@ def defaultButton(width, height, x, y, text, color, hover_color, font):
     else:
         pygame.draw.rect(screen, color, (x, y, width, height))
 
-    text_surface, text_rect = font.render(text, (0, 0, 0), size=40)
+    text_surface, text_rect = font.render(text, (0, 0, 0), size=font_size)
     text_rect.center = (x + width / 2, y + height / 2)
     screen.blit(text_surface, text_rect)
 
@@ -105,7 +87,7 @@ while running:
 
     # Draw the button
     if button_visible:
-        defaultButton(200, 50, (width - 200)//2, (height - 50)//2, "Play", ("white"), (0, 200, 0), gameFont)
+        defaultButton(200, 50, (width - 200)//2, (height - 50)//2, "Play", ("white"), (0, 200, 0), gameFont, 40)
         defaultDisplay(600, 200, (width - 600)//2, (height - 600)//2, "NihonGo", ("light green"), gameFont2, 90, "black")
 
     elif not button_visible:
@@ -113,26 +95,46 @@ while running:
         #To implement this we may need to make the button function to return a value when it is clicked either true or false or we could add another function for 
         #buttons that return a value when clicked
 
-        defaultButton(300, 150, (width - 500)//2, (height + 500)//2, "Default4", ("white"), (0, 200, 0), gameFont) #Bottom Right
-        defaultButton(300, 150, (width - 1400)//2, (height + 500)//2, "Default3", ("white"), (0, 200, 0), gameFont) #Bottom Left
-        defaultButton(300, 150, (width - 500)//2, (height - 150)//2, "Default2", ("white"), (0, 200, 0), gameFont) #Top Right
-        defaultButton(300, 150, (width - 1400)//2, (height - 150)//2, "Default1", ("white"), (0, 200, 0), gameFont) #Bottom Left
+        while count != 2:
+            correlatedIndex = random.randint(0, len(englishDictionary))
+            while correlatedIndex == historyIndex:
+                correlatedIndex = random.randint(0, len(englishDictionary))
+            print(imageHash[correlatedIndex])
+            print(japaneseDictionary[correlatedIndex])
 
-        defaultButton(300, 100, (width - 950)//2, (height - 550)//2, "Hint?", ("white"), (0, 200, 0), gameFont)#Hint button
+            option1 = random.randint(0, len(englishDictionary) - 1)
+            option2 = random.randint(0, len(englishDictionary) - 1)
+
+            while option1 == correlatedIndex or option1 == historyIndex:
+                option1 = random.randint(0, len(englishDictionary) - 1)
+            while option2 == correlatedIndex or option2 == option1 or option2 == historyIndex:
+                option2 = random.randint(0, len(englishDictionary) - 1)
+
+            print("Which of the following is the correct translation of the word above?")
+
+            possibleAnswers = [englishDictionary[correlatedIndex],englishDictionary[option1], englishDictionary[option2]]
+            random.shuffle(possibleAnswers)
+
+            count += 1
+            historyIndex = correlatedIndex
+
+            #pygame.time.wait(2000)
+
+        defaultButton(300, 150, (width - 500)//2, (height + 500)//2, japaneseDictionary[correlatedIndex], ("white"), (0, 200, 0), gameFont, 40) #Bottom Right
+        defaultButton(300, 150, (width - 1400)//2, (height + 500)//2, "Default3", ("white"), (0, 200, 0), gameFont, 40) #Bottom Left
+        defaultButton(300, 150, (width - 500)//2, (height - 150)//2, "Default2", ("white"), (0, 200, 0), gameFont, 40) #Top Right
+        defaultButton(300, 150, (width - 1400)//2, (height - 150)//2, "Default1", ("white"), (0, 200, 0), gameFont, 40) #Bottom Left
+        
+        defaultButton(300, 100, (width - 950)//2, (height - 650)//2, "Hint?", ("white"), (0, 200, 0), gameFont, 40)#Hint button
+        defaultDisplay(700, 100, (width - 1350)//2, (height - 400)//2, "Which is the correct translation of the word on the right?", ("white"), gameFont, 25, "black")#Hint button
 
         defaultDisplay(300, 100, (width - 1400)//2, height - 1050 , "Streak:", ("white"), gameFont, 40, "black")#Streak Button
         defaultDisplay(300, 100, (width - 500)//2, height - 1050 , "Top Streak:", ("white"), gameFont, 40, "black")#High Score Button
-
-        #we will probably use this later
-        #if hint:
+        
         imageDisplay(550, 500, (width + 350)//2, height - 630, "Images/answers/ocean.jpg")
 
-        #Japaneses Characters
         defaultDisplay(550, 100, (width +350)//2, height - 1050 , "Hiragana", ("white"), gameFont, 40, "black")#High Score Button
         defaultDisplay(550, 300, (width +350)//2, height - 940 , "Japanese", ("white"), gameFont, 80, "black")#High Score Button
-
-
-
 
     pygame.display.flip()
     clock.tick(60)
